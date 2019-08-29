a2e562e6e37568beef28b18db6a0a3a862dc5d5a
fedi <fedi.dayeg@hotmail.com>
2019-08-29 18:40:03 +0100

    Ajoutez des fichiers projet.

---
+++ centre de stage chebba/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace centre_de_stage_chebba
+{
+    /// <summary>
+    /// Logique d'interaction pour App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
