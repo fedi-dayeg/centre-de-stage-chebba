e5cc8702ff5d7c2a00f662d57e4fbe435e560b56
fedi <fedi.dayeg@hotmail.com>
2019-09-02 00:48:04 +0100

    final the connxion class

--- centre de stage chebba/Connexion.cs
+++ centre de stage chebba/Connexion.cs
@@ -4,6 +4,7 @@ using System.Data.OleDb;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static centre_de_stage_chebba.CustomException;
 
 namespace centre_de_stage_chebba
 {
@@ -21,10 +22,10 @@ namespace centre_de_stage_chebba
             catch(Exception e)
             {
                 oleDbConnection.Close();
-                
+                throw new CustomException("Le système ne peut pas se connecter à la base de données. Veuillez contacter votre administrateur système.", ExceptionType.Error, "Error message:" + e.Message);
             }
             return oleDbConnection;
-
+            
         }
     }
 }
---
+++ centre de stage chebba/CustomException.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace centre_de_stage_chebba
+{
+    class CustomException : ApplicationException
+    {
+
+        public enum ExceptionType
+        {
+            Info, Warn, Error
+        }
+        public ExceptionType Type
+        {
+            get;
+            set;                                        
+        }
+        
+        public string DetailMessage
+        {
+            get;
+            set;
+        }
+
+        public CustomException (string message) : base(message)
+        {
+            Type = ExceptionType.Warn;
+        }
+
+        public CustomException(string message, ExceptionType type, string detailMessage)
+            : base(message)
+        {
+            Type = type;
+            DetailMessage = detailMessage;
+        }
+    
+}
+}
