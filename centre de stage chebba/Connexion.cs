a6c15da0ee0c7e1a053b716ecf4ca808865821ab
fedi <fedi.dayeg@hotmail.com>
2019-09-02 00:26:38 +0100

    add the Connexion Class

---
+++ centre de stage chebba/Connexion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace centre_de_stage_chebba
+{
+    class Connexion
+    {
+        private static OleDbConnection GetNewConnection()
+        {
+            OleDbConnection oleDbConnection = new OleDbConnection();
+            oleDbConnection.ConnectionString = System.Configuration.ConfigurationManager.AppSettings["accessServerConnString"];
+
+            try
+            {
+                oleDbConnection.Open();
+            }
+            catch(Exception e)
+            {
+                oleDbConnection.Close();
+                
+            }
+            return oleDbConnection;
+
+        }
+    }
+}
